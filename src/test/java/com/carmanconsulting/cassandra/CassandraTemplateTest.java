1c3673cde792b2c067f01af12e8e49402d34a84f
James Carman <jcarman@apache.org>
2014-05-08 17:00:03 -0400

    Removing cruft.

--- src/test/java/com/carmanconsulting/cassandra/CassandraTemplateTest.java
+++ src/test/java/com/carmanconsulting/cassandra/CassandraTemplateTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.List;
 import static org.junit.Assert.*;
+
 public class CassandraTemplateTest extends CassandraTestCase {
 //----------------------------------------------------------------------------------------------------------------------
 // Other Methods
